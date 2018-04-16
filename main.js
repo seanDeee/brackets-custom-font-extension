3aa968b548fa5c1c9ae67a7f26b25c55716c47b8
Sean Dee <37123594+seanDeee@users.noreply.github.com>
2018-04-17 01:00:15 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+define(function (require, exports, module) {
+    "use strict";
+    var ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
+        CommandManager = brackets.getModule("command/CommandManager"),
+        Menus = brackets.getModule("command/Menus"),
+        Commands = brackets.getModule("command/Commands"),
+        EditorManager = brackets.getModule("editor/EditorManager"),
+        FileUtils = brackets.getModule("file/FileUtils"),
+        Dialogs = brackets.getModule("widgets/Dialogs"),
+        PreferencesManager = brackets.getModule("preferences/PreferencesManager");
+
+
+    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror { font-family: "
+        Times New Roman ", Times; }");
+
+
+
+
+
+
+
+
+
+});
